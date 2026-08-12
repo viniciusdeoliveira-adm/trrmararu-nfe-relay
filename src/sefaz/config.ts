5b4f4cc26b52b1662320bec4a5ac0f603dcb73b9
viniciusdeoliveira-adm <viniciusdeoliveira.adm@gmail.com>
2026-08-12 18:21:36 -0300

    feat: add SEFAZ PA homologation configuration

---
+++ src/sefaz/config.ts
@@ -0,0 +1,7 @@
+export const SEFAZ_CONFIG = {
+  uf: "PA",
+  ambiente: "homologacao",
+  versao: "4.00",
+  statusServicoUrl:
+    "https://nfe-homologacao.svrs.rs.gov.br/ws/NfeStatusServico/NfeStatusServico4.asmx",
+} as const;
